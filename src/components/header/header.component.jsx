401b13947730127240b37f2fdd99e7a4cd100cf7
SimDebug27 <simonneyt@hotmail.com>
2020-12-11 23:50:54 +0100

    moved directory, shop data in own reducer, created respective selectors

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -25,13 +25,12 @@ const Header = ({ currentUser, hidden }) => (
             <Link className='option' to='/contact'>
                 CONTACT
             </Link>
-            {
-                currentUser ?
-                <div className='option' onClick={() => auth.signOut()}>SIGN OUT</div>
-                :
-                <Link className='option' to='/signin'>
-                    SIGN IN
-                </Link>
+            {currentUser ? 
+                <div className='option' onClick={() => auth.signOut()}>
+                    SIGN OUT
+                </div>
+                : 
+                <Link className='option' to='/signin'>SIGN IN</Link>
             }
             <CartIcon />
         </div>
