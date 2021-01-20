8394b2487eb214f2a2fb5b3034122c9aca7190ae
SimDebug27 <simonneyt@hotmail.com>
2021-01-20 21:26:44 +0100

    updated App componenet, sign-in and sign-ip with Hooks

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
 
 import  FromInput from '../form-input/form-input.component';
@@ -12,62 +12,53 @@ import {
     ButtonsBarContainer
 } from './sign-in.styles';
 
-class SignIn extends React.Component{
-    constructor(props) {
-        super(props);
+const SignIn = ({ emailSignInStart, googleSignInStart }) => {
+    const [userCredentials, setCredentials ] = useState({ email: '', password: ''})
 
-        this.state = {
-            email: '',
-            password: ''
-        }
-    }
-
-    handleSubmit = async event => {
+    const { email, password } = userCredentials;
+    
+    const handleSubmit = async event => {
         event.preventDefault();
-        const { emailSignInStart } = this.props;
-        const { email, password } = this.state;
 
         emailSignInStart(email, password);
     }
 
-    handleChange = event => {
+    const handleChange = event => {
         const { value, name } = event.target;
 
-        this.setState({ [name]: value})
+        setCredentials({...userCredentials, [name]: value})
     };
 
-    render() {
-        const { googleSignInStart } = this.props;
-        return(
-            <SignInContainer>
-                <SignInTitle>I already have an account</SignInTitle>
-                <span>Sign in with your email and pasword</span>
+    
+    return(
+        <SignInContainer>
+            <SignInTitle>I already have an account</SignInTitle>
+            <span>Sign in with your email and pasword</span>
 
-                <form onSubmit={this.handleSubmit}>
-                    <FromInput 
-                        name='email' 
-                        type='email' 
-                        value={this.state.email} 
-                        handleChange={this.handleChange} 
-                        label='email' 
-                        required 
-                    />
-                    <FromInput 
-                        name='password' 
-                        type='password' 
-                        value={this.state.password} 
-                        handleChange={this.handleChange} 
-                        label='password' 
-                        required 
-                    />
-                    <ButtonsBarContainer>
-                        <CustomButton type='submit'>Sign In</CustomButton>
-                        <CustomButton type='button' onClick={googleSignInStart} isGoogleSignIn>Sign In With Google</CustomButton>
-                    </ButtonsBarContainer>
-                </form>
-            </SignInContainer>
-        )
-    }
+            <form onSubmit={handleSubmit}>
+                <FromInput 
+                    name='email' 
+                    type='email' 
+                    value={email} 
+                    handleChange={handleChange} 
+                    label='email' 
+                    required 
+                />
+                <FromInput 
+                    name='password' 
+                    type='password' 
+                    value={password} 
+                    handleChange={handleChange} 
+                    label='password' 
+                    required 
+                />
+                <ButtonsBarContainer>
+                    <CustomButton type='submit'>Sign In</CustomButton>
+                    <CustomButton type='button' onClick={googleSignInStart} isGoogleSignIn>Sign In With Google</CustomButton>
+                </ButtonsBarContainer>
+            </form>
+        </SignInContainer>
+    )
 }
 
 const mapDispatchToProps = dispatch => ({
